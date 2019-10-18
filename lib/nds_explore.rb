adbd0721df291359d3a9dd4bba98227a9299a3e3
Nory Lazo <nlazo@icloud.com>
2019-10-18 08:41:45 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -43,7 +43,7 @@ def print_first_directors_movie_titles
 movie_index = 0 
 
   while movie_index < directors_database[0][:movies].length do 
-    pp directors_database[0][:movies][movie_index][:title]
+    puts directors_database[0][:movies][movie_index][:title]
     
     movie_index += 1 
   end 
